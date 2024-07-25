fe064cbf09338cfd136ba0193f583112e288fab4
NATTAPON TANCHO <b_o_y_s@hotmail.com>
2024-07-26 01:44:06 +0700

    initial testing file

---
+++ pf-testing/cypress/e2e/spec.cy.ts
@@ -0,0 +1,28 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
+
+describe("Backend", () => {
+  it("checks get response", () => {
+    const url = "http://localhost:3000";
+    cy.request({
+      method: "GET",
+      url: `${url}/todo`,
+    }).then((res) => {
+      expect(res.body).to.be.a("array");
+    });
+  });
+});
+
+describe("Frontend", () => {
+  it("creates todo", () => {
+    const url = "http://localhost:5173";
+    const text = new Date().getTime().toString();
+    cy.visit(url);
+    cy.get("[data-cy='input-text']").type(text);
+    cy.get("[data-cy='submit']").click();
+    cy.contains(text);
+  });
+});
