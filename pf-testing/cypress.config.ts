7f57d029a77bb57aaa11441ade3ed4832e933aed
bukbunny <khomsan_su@cmu.ac.th>
2024-07-28 19:35:57 +0700

    Add Test API

--- pf-testing/cypress.config.ts
+++ pf-testing/cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "cypress";
+import "dotenv/config";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env = config.env || {};
+      // you could extract only specific variables and rename them if necessary
+      config.env.FRONTEND_URL = process.env.FRONTEND_URL;
+      config.env.BACKEND_URL = process.env.BACKEND_URL;
+      console.log("Extended config.env with process.env");
+      return config;
     },
   },
 });
--- pf-testing/cypress/e2e/spec.cy.ts
+++ pf-testing/cypress/e2e/spec.cy.ts
@@ -5,24 +5,26 @@ describe("template spec", () => {
 });
 
 describe("Backend", () => {
+
   it("checks get response", () => {
     const url = "http://localhost:3000";
     cy.request({
       method: "GET",
-      url: `${url}/todo`,
+      url: `${url}/todolist`,
     }).then((res) => {
       expect(res.body).to.be.a("array");
     });
   });
 });
 
-describe("Frontend", () => {
-  it("creates todo", () => {
-    const url = "http://localhost:5173";
-    const text = new Date().getTime().toString();
-    cy.visit(url);
-    cy.get("[data-cy='input-text']").type(text);
-    cy.get("[data-cy='submit']").click();
-    cy.contains(text);
-  });
-});
+
+// describe("Frontend", () => {
+//   it("creates todo", () => {
+//     const url = "http://localhost:5173";
+//     const text = new Date().getTime().toString();
+//     cy.visit(url);
+//     cy.get("[data-cy='input-text']").type(text);
+//     cy.get("[data-cy='submit']").click();
+//     cy.contains(text);
+//   });
+// });
